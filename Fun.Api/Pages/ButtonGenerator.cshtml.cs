2019113285146bc9f1d7637e17a07905c0c68c6d
Tim van Heumen <tim@vhweb.nl>
2020-12-07 14:32:49 +0100

    Added Nr of columns option

--- Fun.Api/Pages/ButtonGenerator.cshtml.cs
+++ Fun.Api/Pages/ButtonGenerator.cshtml.cs
@@ -13,6 +13,23 @@ namespace Fun.Api.Pages
         [BindProperty]
         public string ApiKey { get; set; }
 
+        [BindProperty]
+        public int NrOfColumns { get; set; }
+
+        public string GridTemplateColumnsCss
+        {
+            get
+            {
+                var percentage = 100 / NrOfColumns;
+                var gridTemplateColumnsCss = string.Empty;
+                for (int i = 0; i < NrOfColumns; i++)
+                {
+                    gridTemplateColumnsCss += percentage.ToString() + "% ";
+                }
+                return gridTemplateColumnsCss;
+            }
+        }
+
         public string BaseUrl { get; set; }
 
         public ActionResult OnGet()
@@ -23,6 +40,7 @@ namespace Fun.Api.Pages
         public void OnPost()
         {
             BaseUrl = $"{Request.Scheme}://{Request.Host}";
+            NrOfColumns = NrOfColumns < 1 ? NrOfColumns = 1 : NrOfColumns;
         }
     }
 }
