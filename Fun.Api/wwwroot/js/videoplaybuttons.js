91394e7bc643c47898f0a5bdae52ec16596cf65e
Tim van Heumen <tim@vhweb.nl>
2021-04-05 12:38:15 +0200

    reorganized css and js files

--- Fun.Api/wwwroot/js/videoplaybuttons.js
+++ Fun.Api/wwwroot/js/videoplaybuttons.js
@@ -1,60 +1,4 @@
-﻿class VideoPlayer {
-    constructor() {
-        this.connected = false;
-        this.connection = new signalR.HubConnectionBuilder().withUrl("/videohub").build();
-        this.connectButton = document.getElementById('connection-btn');
-        this.videoplayer = document.getElementById('videoplayer');
-
-        this.connection.on('PlayVideo', this.play);
-        this.connection.on('StopVideo', this.stop);
-
-        var connetionButton = document.querySelector("#connection-btn");
-        connetionButton.onclick = this.connection_onclick;
-    }
-
-    play = (url) => {
-        if (this.videoplayer.paused) {
-            this.videoplayer.src = url;
-            this.videoplayer.play();
-        }
-    }
-
-    stop = () => {
-        this.videoplayer.pause();
-    }
-
-    connection_onclick = () => {
-        if (!this.connected) {
-            this.connect();
-        } else {
-            this.disconnect();
-        }
-    }
-
-    async connect() {
-        await this.connection.start();
-        this.connected = true;
-
-        this.connectButton.classList.add('btn-outline-danger');
-        this.connectButton.classList.remove('btn-outline-success');
-        this.connectButton.textContent = 'disconnect';
-
-        this.videoplayer.style.visibility = 'visible';
-    }
-
-    async disconnect() {
-        await this.connection.stop();
-        this.connected = false;
-
-        this.connectButton.classList.add('btn-outline-success');
-        this.connectButton.classList.remove('btn-outline-danger');
-        this.connectButton.textContent = 'connect';
-
-        this.videoplayer.style.visibility = 'hidden';
-    }
-}
-
-class VideoPlayButtons {
+﻿class VideoPlayButtons {
     constructor() {
         let videoFilter = document.querySelector("#video-filter");
         videoFilter.oninput = this.filterVideos_oninput;
@@ -87,7 +31,7 @@ class VideoPlayButtons {
     videoButton_onclick = (event) => {
         let url;
         if (event.target.nodeName == "BUTTON") {
-            url = encodeURIComponent(event.target.dataset.videoUrl);           
+            url = encodeURIComponent(event.target.dataset.videoUrl);
         }
         if (event.target.nodeName == "SPAN") {
             url = encodeURIComponent(event.target.parentNode.dataset.videoUrl);
@@ -131,6 +75,5 @@ class VideoPlayButtons {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    new VideoPlayer();
     new VideoPlayButtons();
 });
