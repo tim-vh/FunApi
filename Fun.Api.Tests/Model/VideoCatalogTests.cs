b28ffc5c75e99887cb856b0f3697b46f3b15cfb3
Tim van Heumen <tim@vhweb.nl>
2022-04-01 12:49:37 +0200

    Added VideoCatalogTests

---
+++ Fun.Api.Tests/Model/VideoCatalogTests.cs
@@ -0,0 +1,33 @@
+﻿using Fun.Api.Model;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Fun.Api.UnitTests.Model
+{
+    public class VideoCatalogTests
+    {
+        [Fact]
+        public void AddVideo_stores_youtube_videos_in_the_youtube_repository()
+        {
+            // Arrange
+            var youtubeRepositoryMock = new Mock<IVideoRepository>();
+            youtubeRepositoryMock.SetupGet(y => y.Type).Returns("Youtube");
+            var repositories = new List<IVideoRepository>();
+            repositories.Add(youtubeRepositoryMock.Object);
+
+            var videoCatalog = new VideoCatalog(repositories);
+            var video = new Video
+            {
+                Name = "Test",
+                Url = "https://www.youtube.com/testvideo1"
+            };
+
+            // Act
+            videoCatalog.AddVideo(video);
+
+            // Assert
+            youtubeRepositoryMock.Verify(y => y.AddVideo(video));  
+        }
+    }
+}
